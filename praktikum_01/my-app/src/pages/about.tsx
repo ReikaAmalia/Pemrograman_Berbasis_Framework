a27068f55fdbe186234076a5612288ca7e916be9
rere <reikaamalia17@gmail.com>
2026-02-17 01:43:54 +0700

    initial praktikum framework

---
+++ praktikum_01/my-app/src/pages/about.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>About</h1>
+
+      <p>Nama Mahasiswa : REIKA AMALIA</p>
+      <p>NIM : 2341720173</p>
+      <p>Program Studi : D4 Teknik Informatika</p>
+    </div>
+  );
+}
