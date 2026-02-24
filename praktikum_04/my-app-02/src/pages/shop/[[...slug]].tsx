375035c0d0e16b371f467106b994a064a06493fc
rere <reikaamalia17@gmail.com>
2026-02-24 20:51:44 +0700

    feat: implement login and register pages, and update produk and shop pages with routing

--- praktikum_04/my-app-02/src/pages/shop/[[...slug]].tsx
+++ praktikum_04/my-app-02/src/pages/shop/[[...slug]].tsx
@@ -4,10 +4,16 @@ const halamanToko = () => {
     // const Router = useRouter();
     // console.log(Router);
     const { query } = useRouter();
+    const slug = query.slug as string[] | undefined;
+
 return (
 <div>
     <h1>Halaman Toko</h1>
-    <p>Toko: {`${query.slug && query.slug[0]+"-"+ query.slug[1]}`}</p>  {/* menggunakan backtick bukan petik satu */}
+    <p>Kategori: {slug ? slug[0] : "Semua Kategori"}</p>
+     <p>
+        Toko: {Array.isArray(slug) ? slug.join("-") : "-"}
+      </p>
+    {/* <p>Toko: {`${query.slug && query.slug[0]+"-"+ query.slug[1]}`}</p>  menggunakan backtick bukan petik satu */}
     {/* <p>Toko: {Array.isArray(query.slug) ? query.slug.join("-") : query.slug}</p>  Berapapun banyaknya seqment tetap terbaca  */}
 </div>
     );
