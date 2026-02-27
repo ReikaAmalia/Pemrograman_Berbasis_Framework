b1a2acb2ce4442b226be0ff9233ff8bd2c7193f0
rere <reikaamalia17@gmail.com>
2026-02-27 20:09:28 +0700

    Add custom 404 page and styles
    - Created a new 404.tsx component for handling page not found errors.
    - Added corresponding styles in 404.module.scss for the 404 page layout and image.

---
+++ praktikum_06/my-app-06/src/pages/404.tsx
@@ -0,0 +1,13 @@
+import styles from '@/styles/404.module.scss'
+
+const Custom404 = () => {
+    return (
+        <div className={styles.error}>
+            <img src="/page-not-found.png" alt="404" className={styles.error_image} />
+            <h1>404 - Halaman Tidak Ditemukan</h1>
+            <p>Maaf, halaman yang Anda cari tidak ada.</p>
+        </div>
+    );
+}
+
+export default Custom404;
