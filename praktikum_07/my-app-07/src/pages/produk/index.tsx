a0968928004c4a7fc14f6af2739f0b70543f1538
rere <reikaamalia17@gmail.com>
2026-03-02 23:17:40 +0700

    feat: integrate Firebase for product management
    - Added Firebase SDK to package.json
    - Created API route to fetch products from Firestore
    - Implemented product retrieval logic in servicefirebase.ts
    - Updated produk page to display product list

--- praktikum_07/my-app-07/src/pages/produk/index.tsx
+++ praktikum_07/my-app-07/src/pages/produk/index.tsx
@@ -1,25 +1,52 @@
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import TampilanProduk from "@/views/produk";
 
-const ProdukPage = () => {
+type ProductType = {
+    id: string;
+    name: string;
+    price: number;
+    size: string;
+}
 
-    const { push } = useRouter();
+const kategori = () => {
+    const [products, setProducts] = useState([]);
+    // const ProdukPage = () => {
+    // const { push } = useRouter();
 
     // Cek status login saat halaman dibuka
-    useEffect(() => {
-        const loginStatus = localStorage.getItem("isLogin");
+    // useEffect(() => {
+    //     const loginStatus = localStorage.getItem("isLogin");
 
-        if (loginStatus !== "true") {
-            push("/auth/login");
-        }
+    //     if (loginStatus !== "true") {
+    //         push("/auth/login");
+    //     }
+    // }, []);
+
+    useEffect(() => {
+        fetch("/api/produk")
+            .then((response) => response.json())
+            .then((responsdata) => {
+                //console.log("Data produk:", responsdata.data);
+                setProducts(responsdata.data);
+            })
+            .catch((error) => {
+                console.error("Error fetching produk:", error);
+            });
     }, []);
 
     return (
         <div>
-            <TampilanProduk />
+        <h1>Daftar Produk</h1>
+        {products.map((products: ProductType) => (
+            <div key={products.id}>
+                <h2>{products.name}</h2>
+                <p>Harga: Rp {products.price}</p>
+                <p>Ukuran: {products.size}</p>
+            </div>
+        ))}
         </div>
     );
 };
 
-export default ProdukPage;
+export default kategori;
