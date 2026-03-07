16a1f6e46d02fa2a858da72c5c4c25f4718bb99b
rere <reikaamalia17@gmail.com>
2026-03-07 23:07:59 +0700

    update step 1

--- praktikum_11/my-app-11/src/views/produk/index.tsx
+++ praktikum_11/my-app-11/src/views/produk/index.tsx
@@ -9,53 +9,53 @@ type ProductType = {
   category: string;
 };
 
-const TampilanProduk = ({ products, isLoading, }: { products: ProductType[]; isLoading?: boolean }) => {
+const TampilanProduk = ({ products }: { products: ProductType[] }) => {
   return (
     <div className={styles.produk}>
       <h1 className={styles.produk__title}>Daftar Produk</h1>
 
       <div className={styles.produk__content}>
-
-        {isLoading ? (
+        {products.length > 0 ? (
+          
           <>
-            <div className={styles.produk__content__skeleton}>
-              <div className={styles.produk__content__skeleton__image}></div>
-              <div className={styles.produk__content__skeleton__name}></div>
-              <div className={styles.produk__content__skeleton__category}></div>
-              <div className={styles.produk__content__skeleton__price}></div>
-            </div>
-          </>
-        ) : (
-          products.map((product: ProductType) => (
+          {products.map((product: ProductType) => (
             <Link href={`/produk/${product.id}`} key={product.id} className={styles.produk__content__item}>
-            <div
-              key={product.id}
-              className={styles.produk__content__item}
-            >
-              <div className={styles.produk__content__item__image}>
-                <img
-                  src={product.image}
-                  alt={product.name}
-                  width={200}
-                />
+              <div
+                // key={product.id}
+                // className={styles.produk__content__item}
+              >
+                <div className={styles.produk__content__item__image}>
+                  <img
+                    src={product.image}
+                    alt={product.name}
+                    width={200}
+                  />
+                </div>
+
+                <h4 className={styles.produk__content__item__name}>
+                  {product.name}
+                </h4>
+
+                <p className={styles.produk__content__item__category}>
+                  {product.category}
+                </p>
+
+                <p className={styles.produk__content__item__price}>
+                  Rp {product.price.toLocaleString("id-ID")}
+                </p>
               </div>
-
-              <h4 className={styles.produk__content__item__name}>
-                {product.name}
-              </h4>
-
-              <p className={styles.produk__content__item__category}>
-                {product.category}
-              </p>
-
-              <p className={styles.produk__content__item__price}>
-                Rp {product.price.toLocaleString("id-ID")}
-              </p>
-            </div>
             </Link>
-          ))
-        )}
+          ))}
+        </>
 
+        ) : (
+          <div className={styles.produk__content__skeleton}>
+              <div className={styles.produk__content__skeleton__item__image}> </div> 
+              <div className={styles.produk__content__skeleton__item__name}> </div>
+              <div className={styles.produk__content__skeleton__item__category}> </div>
+              <div className={styles.produk__content__skeleton__item__price}> </div> 
+            </div>
+        )}
       </div>
     </div>
   );
