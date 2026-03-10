95bcda8254018129b49d1b52e40f5f36ee30ae25
rere <reikaamalia17@gmail.com>
2026-03-10 22:58:56 +0700

    JS 13

---
+++ praktikum_13/my-app-13/src/pages/produk/index.tsx
@@ -0,0 +1,20 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import TampilanProduk from "@/views/produk";
+import useSWR from "swr";
+import fetcher from "../../utils/swr/fetcher";
+
+const kategori = () => {
+    const [products, setProducts] = useState([]);
+    const { data, error, isLoading } = useSWR("/api/produk", fetcher);
+
+    return (
+        <div>
+            <TampilanProduk
+            products={data?.data || []}
+            />
+        </div>
+    );
+};
+
+export default kategori;
