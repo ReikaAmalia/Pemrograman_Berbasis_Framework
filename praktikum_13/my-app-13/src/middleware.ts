b908a52713cc414509d1ffcb8d548d5c655f57ac
rere <reikaamalia17@gmail.com>
2026-03-31 15:48:06 +0700

    Bagian 5 – Simulasi Sistem Login

--- praktikum_13/my-app-13/src/middleware.ts
+++ praktikum_13/my-app-13/src/middleware.ts
@@ -2,11 +2,17 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-    return NextResponse.redirect(new URL("/", request.url));
+  const isLogin = true;
+  if (isLogin) {
+    return NextResponse.next();
+  } else {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
 
-    //return NextResponse.next();
+  //return NextResponse.redirect(new URL("/", request.url));
+  //return NextResponse.next();
 }
 
-    export const config = {
-        matcher: "/about/:path*",
-    };
+export const config = {
+  matcher: ["/produk", "/about"],
+};
