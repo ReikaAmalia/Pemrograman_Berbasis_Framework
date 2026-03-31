a44d452d17112aa9665bf533fd9169ef2e5c44ca
rere <reikaamalia17@gmail.com>
2026-03-31 14:27:45 +0700

    Bagian 1 – Membuat Middleware

--- praktikum_13/my-app-13/src/pages/produk/index.tsx
+++ praktikum_13/my-app-13/src/pages/produk/index.tsx
@@ -5,14 +5,17 @@ import useSWR from "swr";
 import fetcher from "../../utils/swr/fetcher";
 
 const kategori = () => {
+    const { push } = useRouter();
     const [products, setProducts] = useState([]);
+    
+    
     const { data, error, isLoading } = useSWR("/api/produk", fetcher);
+    // cek apakah data, error, dan isLoading sudah bnar
 
     return (
         <div>
             <TampilanProduk
-            products={data?.data || []}
-            />
+            products={isLoading ? [] : data.data} />
         </div>
     );
 };
