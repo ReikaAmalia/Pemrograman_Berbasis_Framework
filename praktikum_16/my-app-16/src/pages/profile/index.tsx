aab461bb41247b6088b10519d39685d2b7adbd83
rere <reikaamalia17@gmail.com>
2026-04-02 20:15:03 +0700

    js 16

---
+++ praktikum_16/my-app-16/src/pages/profile/index.tsx
@@ -0,0 +1,13 @@
+import { useSession } from 'next-auth/react';
+
+const ProfilePage = () => {
+  const { data }: any = useSession();
+  return (
+    <div>
+      <h1>Halaman Profile</h1> <br />
+      <h1>Selamat Datang {data?.user?.fullname}</h1>
+    </div>
+  );
+};
+
+export default ProfilePage;
---
+++ praktikum_16/my-app-16/src/views/auth/register/index.tsx
@@ -0,0 +1,139 @@
+import Link from "next/link";
+import style from "../../auth/register/register.module.scss";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+const TampilanRegister = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { push } = useRouter();
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    setError("");
+    setIsLoading(true);
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email") as string;
+    const fullname = formData.get("Fullname") as string;
+    const password = formData.get("Password") as string;
+
+    // validasi email dan password
+    if (!email) {
+      setError("Email wajib diisi");
+      setIsLoading(false);
+      return; // ← stop, tidak lanjut ke fetch
+    }
+    if (password.length < 6) {
+      setError("Password minimal 6 karakter");
+      setIsLoading(false);
+      return; // ← stop, tidak lanjut ke fetch
+    }
+
+    const response = await fetch("/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, fullname, password }),
+    });
+    // const result = await response.json();
+    // console.log(result);
+    if (response.status === 200) {
+      form.reset();
+      // event.currentTarget.reset();
+      setIsLoading(false);
+      push("/auth/login");
+    } else {
+      setIsLoading(false);
+      setError(
+        response.status === 400 ? "Email already exists" : "An error occurred",
+      );
+    }
+  };
+  
+  return (
+    <div className={style.register}>
+      {/* modifikasi line 41 */}
+      {error && <p className={style.register__form__error}>{error}</p>}
+      <h1 className={style.register__title}>Halaman Register</h1>
+
+      <div className={style.register__form}>
+
+        <form onSubmit={handleSubmit}>
+
+          {/* Email */}
+          <div className={style.register__form__item}>
+            <label
+              htmlFor="email"
+              className={style.register__form__item__label}
+            >
+              Email 
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
+              className={style.register__form__item__input}
+            />
+          </div>
+
+          {/* Fullname */}
+          <div className={style.register__form__item}>
+            <label
+              htmlFor="Fullname"
+              className={style.register__form__item__label}
+            >
+              Fullname
+            </label>
+            <input
+              type="text"
+              id="Fullname"
+              name="Fullname"
+              placeholder="Fullname"
+              className={style.register__form__item__input}
+            />
+          </div>
+
+          {/* Password */}
+          <div className={style.register__form__item}>
+            <label
+              htmlFor="Password"
+              className={style.register__form__item__label}
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="Password"
+              name="Password"
+              placeholder="Password"
+              className={style.register__form__item__input}
+            />
+          </div>
+
+          {/* Button Register */}
+          <button
+            type="submit"
+            className={style.register__form__item__button}
+            disabled={isLoading}
+          >
+            {isLoading ? "Loading..." : "Register"}
+          </button>
+
+        </form>
+
+        {/* Halaman login */}
+        <br />
+        <p className={style.register__form__item__text}>
+          Sudah punya akun? <Link href="/auth/login">Ke Halaman Login</Link>
+        </p>
+      </div>
+      {/* Form selesai disini */}
+
+    </div>
+  );
+};
+
+export default TampilanRegister;
