b28b317ee5cda920df2bb1cafbb026174a927d40
rere <reikaamalia17@gmail.com>
2026-04-04 19:27:20 +0700

    PRAKTIKUM 1 – Image Optimization A. Optimasi Gambar Lokal (Public Folder)

--- praktikum_18/my-app-18/src/pages/404.tsx
+++ praktikum_18/my-app-18/src/pages/404.tsx
@@ -1,40 +1,53 @@
-import Head from "next/head";
+import Head from "next/image";
 import styles from "@/styles/404.module.scss";
 // TUGAS 3: KEMBALI KE HOME
 import Link from "next/link";
+import Image from "next/image";
 
 const Custom404 = () => {
   return (
     <>
-      <Head>
+      {/* <Head>
         <title>404 - Halaman Tidak Ditemukan</title>
         <meta
           name="description"
           content="Halaman yang Anda cari tidak tersedia."
         />
-      </Head>
+      </Head> */}
 
       <div className={styles.error}>
-        <h1 className={styles.error_title}>
+        {/* <h1 className={styles.error_title}>
           404 - Halaman Tidak Ditemukan
         </h1>
 
         <p className={styles.error_desc}>
           Maaf, halaman yang Anda cari mungkin telah dipindahkan atau tidak tersedia.
-        </p>
+        </p> */}
 
-        <img
+        {/* <img
           src="/page-not-found.png"
           alt="Halaman tidak ditemukan"
           className={styles.error_image}
-        />
+        /> */}
+
+          <Image
+          src="/page-not-found.png"
+          alt="404"
+          width={400}
+          height={200}
+          className={styles.error_image}
+          />
+
+          <h1>404 - Halaman Tidak Ditemukan</h1>
+          <p>Maaf, halaman yang Anda cari mungkin telah dipindahkan atau tidak tersedia.</p>
+
 
     {/* TUGAS 3: TOMBOL KEMBALI KE HOME */}
-        <Link href="/">
+        {/* <Link href="/">
         <button className={styles.back_button}>
             Kembali ke Home
         </button>
-        </Link>
+        </Link> */}
 
       </div>
     </>
