eeaba5b433a699f28afc322632e417ca5e859f69
rere <reikaamalia17@gmail.com>
2026-04-04 20:04:10 +0700

    PRAKTIKUM 2 – Font Optimization A. Menggunakan next/font

--- praktikum_18/my-app-18/next.config.js
+++ praktikum_18/my-app-18/next.config.js
@@ -1,22 +1,43 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, 
+  reactStrictMode: true,
   images: {
     remotePatterns: [
+      // ← sudah ada sebelumnya
       {
         protocol: "https",
         hostname: "assets.adidas.com",
         port: "",
         pathname: "/**",
       },
+      // ← sudah ada sebelumnya
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
         pathname: "/**",
       },
+      // ← sudah ada sebelumnya
       {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
+        pathname: "/**",
+      },
+      // ← tambahkan untuk gambar dari Shopee
+      {
+        protocol: "https",
+        hostname: "down-id.img.susercontent.com",
+        pathname: "/**",
+      },
+      // ← tambahkan untuk gambar dari Pinterest
+      {
+        protocol: "https",
+        hostname: "i.pinimg.com",
+        pathname: "/**",
+      },
+      // ← tambahkan untuk gambar dari Nike
+      {
+        protocol: "https",
+        hostname: "static.nike.com",
         pathname: "/**",
       },
     ],
--- praktikum_18/my-app-18/src/components/Appshell/index.tsx
+++ praktikum_18/my-app-18/src/components/Appshell/index.tsx
@@ -1,11 +1,17 @@
 import Navbar from "@/components/navbar";
 import { useRouter } from "next/router";
+import { Roboto } from "next/font/google";
 
 // Daftar path yang tidak menampilkan Navbar
 const disableNavbar = ["/auth/login", "/auth/register", "/404"];
 type AppShellProps = {
     children: React.ReactNode;
 }
+
+const roboto = Roboto({
+    subsets: ["latin"],
+    weight: ["400", "500", "700"],
+});
 
 const AppShell = (props:AppShellProps) => {
     const { children } = props;
@@ -14,15 +20,13 @@ const AppShell = (props:AppShellProps) => {
     console.log(router);
     
     return (
-        <main>
+        <main className={roboto.className}>
             {!disableNavbar.includes(pathname) && <Navbar />}
         {children}
-        <footer style={{ marginTop: "20px" }}>
+        {/* <footer style={{ marginTop: "20px" }}>
             <hr />
             <p> © 2026 - Praktikum Next.js</p>
-        </footer>
-        <div>
-        </div>
+        </footer> */}
         </main>
     );
     };
