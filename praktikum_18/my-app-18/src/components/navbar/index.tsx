af84e65ecb6723538b458e83b104a01a2d46cd8d
rere <reikaamalia17@gmail.com>
2026-04-04 20:11:33 +0700

    PRAKTIKUM 3 – Script Optimization

--- praktikum_18/my-app-18/src/components/navbar/index.tsx
+++ praktikum_18/my-app-18/src/components/navbar/index.tsx
@@ -1,32 +1,34 @@
 import styles from './navbar.module.css';
 import { signIn, signOut, useSession } from 'next-auth/react';
-import Image from 'next/image'; 
+import Image from 'next/image';
+import Script from 'next/dist/client/script';
 
 const Navbar = () => {
-  const {data}: any = useSession()
-  //const { data: session } = useSession()
-  // console.log("session", session)
+  const { data }: any = useSession();
+
   return (
     <div className={styles.navbar}>
-      <div className={styles.navbar__brand}>
-        MyApp
-      </div>
+
+      <div className={styles.navbar__brand} id="title"></div>
+      <Script id="title-script" strategy='lazyOnload'>
+        {`document.getElementById('title').textContent = 'MyApp';`}
+      </Script>
 
       <div className={styles.navbar__right}>
         {data ? (
           <>
             <div className={styles.navbar__user}>
-              Welcome, {data.user?.fullname || data.user?.name} {/* show full name  */}
+              Welcome, {data.user?.fullname || data.user?.name}
               {data.user.image && (
-              <Image
-                src={data.user.image}
-                alt="avatar"
-                width={32}
-                height={32}
-                referrerPolicy="no-referrer"
-                className={styles.navbar__user__image}
-              />
-            )}
+                <Image
+                  src={data.user.image}
+                  alt="avatar"
+                  width={32}
+                  height={32}
+                  referrerPolicy="no-referrer"
+                  className={styles.navbar__user__image}
+                />
+              )}
             </div>
             <button
               className={`${styles.navbar__button} ${styles["navbar__button--danger"]}`}
@@ -44,6 +46,7 @@ const Navbar = () => {
           </button>
         )}
       </div>
+
     </div>
   );
 };
